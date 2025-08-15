c8373151b5f10928ca199be458909d66b5f7d230
vashista <82438452+vashista143@users.noreply.github.com>
2025-08-15 23:56:16 +0530

    edited signuppage

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -28,7 +28,7 @@ const SignupPage = () => {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://panscience-assignment-nfvf.onrender.com/api/auth/signup", {
         method: "POST",
         credentials: "include",
         headers: {
