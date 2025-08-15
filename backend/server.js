f0ec81d291ea7a576ea9708600315479ad4f6d89
vashista <82438452+vashista143@users.noreply.github.com>
2025-08-15 23:58:04 +0530

    edited server

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors"
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://panscience-assignment-ten.vercel.app/loginlocalhost:5173", 
     credentials: true,
   })
 );
